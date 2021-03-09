a05d0f77a2a4a7667e572147331de3cec4e55606
Rick.CC <rickjung88@gmail.com>
2021-03-08 20:54:45 -0700

    Checkpoint

--- eva_t5_qg.py
+++ eva_t5_qg.py
@@ -2,7 +2,9 @@ from pathlib import Path
 
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-path_model = Path('Models', f'tuned_allquests_t5-small')
+# path_model = Path('Models', f'tuned_what_t5-small')
+path_model = 't5-small'
+
 model = T5ForConditionalGeneration.from_pretrained(path_model)
 tokenizer = T5Tokenizer.from_pretrained(path_model)
 
@@ -14,12 +16,14 @@ def seq2seq(input_text):
     return output_text
 
 
-test_texts = {
+test_texts = [
     'Java performs floating-point division when one or more operands are double values.',
     'Conveniently, the code for displaying a variable is the same regardless of its type.',
     'The following program converts a time of day to minutes.',
     'The value of minute is 59, and 59 divided by 60 should be 0.98333, not 0.',
-}
+    'Diagrams like this one that show the state of the program are called memory diagrams.',
+    'There\'s nothing wrong with a method like printTime, but it is not consistent with object-oriented style. A more idiomatic solution is to provide a special method called toString.'
+]
 
 for i in test_texts:
     print('context:', i, '\nquestion', seq2seq(i), '\n')
--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,17 @@
 """
 Clean and preprocess some datasets for the study.
+This script is not about producing data for an nn.Module.
+You can find more detail for that case in `nn_data_processor.py`.
 """
 
 from pathlib import Path
 
-path_thinkjava2 = Path()
+path_thinkjava2 = Path("Datasets", "ThinkJava2-master")
+path_chapters = {
+    'variable': Path(path_thinkjava2, 'ch02.tex'),
+    'io': Path(path_thinkjava2, 'ch03.tex'),
+    'methods_testing': Path(path_thinkjava2, 'ch04.tex'),
+    'conditionals': Path(path_thinkjava2, 'ch05.tex'),
+    'loops_strings': Path(path_thinkjava2, 'ch06.tex'),
+    'arrays_references': Path(path_thinkjava2, 'ch07.tex'),
+}
